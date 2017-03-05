a0c1ca0c4003b8b97c87eb3ab70933ea6744a099
guxuefei <xuefeigu@163.com>
2017-03-05 21:40:07 +0800

    Initial Commit

---
+++ 043-- 自定义转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  043-- 自定义转场动画
+//
+//  Created by 顾雪飞 on 17/3/5.
+//  Copyright © 2017年 顾雪飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 043-- 自定义转场动画/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  043-- 自定义转场动画
+//
+//  Created by 顾雪飞 on 17/3/5.
+//  Copyright © 2017年 顾雪飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
